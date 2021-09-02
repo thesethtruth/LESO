6f181eb54d83f352cb684dc4d4dcf6119f8ec882
thesethtruth <s.w.p.vanwieringen@gmail.com>
2021-09-02 09:39:50 +0200

    cablepool_ema 200 scenarios 2 policies

--- thesis scripts/experiments/cablepool_ema_experiments.py
+++ thesis scripts/experiments/cablepool_ema_experiments.py
@@ -5,7 +5,7 @@ from ema_workbench import perform_experiments, save_results
 from cablepool_ema_model_definition import model
 
 # run experiments
-results = perform_experiments(model, scenarios=2, policies=1)
+results = perform_experiments(model, scenarios=200, policies=2)
 # save results
 results_file_name = os.path.join(RESULT_FOLDER, f"cabelpooling_ema_results_{RUNID}.tar.gz")
 save_results(results, file_name=results_file_name)

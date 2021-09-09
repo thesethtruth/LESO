b2f521c65595d6001e5d1e8dbbf10dd028021aa8
thesethtruth <seth@uu-engineering.nl>
2021-09-09 13:48:28 +0200

    Updated quickbrowser to be able to deal with experiments

--- thesis scripts/analysis/outdated_but_useful/quickbrowser.py
+++ thesis scripts/analysis/outdated_but_useful/quickbrowser.py
@@ -1,72 +1,106 @@
 # quickbrowser.py
+from re import template
+from LESO.attrdict import AttrDict
+from LESO.experiments.analysis import load_ema_leso_results
 import appfunctions as af
 
 import json
 import dash
-import dash_core_components as dcc
-import dash_html_components as html
+from dash import dcc
+from pathlib import Path
 from dash.dependencies import Input, Output
+from dash import html
 import plotly.graph_objects as go
 import dash_bootstrap_components as dbc
+import plotly.express as px
+import pandas as pd
 
-external_stylesheets = ['https://codepen.io/chriddyp/pen/bWLwgP.css']
-app = dash.Dash(__name__, 
-                external_stylesheets=external_stylesheets,
-                title= 'LESO results browser'
-                )
+external_stylesheets = ["https://codepen.io/chriddyp/pen/bWLwgP.css"]
+app = dash.Dash(
+    __name__, external_stylesheets=external_stylesheets, title="LESO results browser"
+)
 
-app.layout = html.Div([
-            html.H3("Results quick browsing analysis"),
-            html.P("Select a simulation run"),
-            dcc.Dropdown(
-                id= 'selected-model',
-                options = [{'label': key, 'value': value}
-                            for key, value in af.load_component_options().items()],
-                value = list(af.load_component_options().values())[0],
-                persistence = False
-            ),
-            html.P("Select a component to assess"),
-            dcc.Dropdown(
-                id = 'selected-component',
-                value = None, 
-                persistence = False,
-            ),
-            dbc.Table(id='component-table'),
-            dcc.Graph(id="hourly"),
-            html.P(['Browse through the year per weeknumber below:'
-                ], style= {"padding-left": "5%"}),
-            dcc.Slider(
-                id='startingweek',
-                min = min(af.weeks.values()),
-                max = max(af.weeks.values()),
-                value= af.startingweek,
-                persistence=False
-            ),
-            html.H3(
-                id="figure-2-title",
-                style={
-                    'textAlign': 'center',
-                }),
-            dcc.Graph(id="figure-2"),
-            html.P("Select a figure to display"),
-            dcc.Dropdown(
-                id= 'selected-figure-2',
-                options = [{'label': value['display-text'], 'value': key}
-                            for key, value in af.figure_2_options.items()],
-                value = list(af.figure_2_options.keys())[0],
-                persistence = False
-            ),
-            dcc.Store(
-                id='data-store',
-                data = dict(),
-            ),
-], className= "container")
+FOLDER = Path(r"C:\Users\Sethv\#Universiteit Twente\GIT\LESO\thesis scripts\experiments\evhub\results")
+*_, df = load_ema_leso_results(
+    run_id=210907, 
+    exp_prefix='evhub',
+    results_folder=FOLDER)
 
-## reads associated JSON file and stores to react app 
+app.layout = html.Div(
+    [
+        html.H3("Results quick browsing analysis"),
+        dcc.Markdown("Select a **primary** feature to filter:"),
+        dcc.Dropdown(
+            id="filter-select-A",
+            options=[
+                {"label": key, "value": key}
+                for key in df.columns if df[key].dtype == 'float64'
+            ],
+            value=df.columns[0],
+            persistence=True,
+        ),
+        dcc.RangeSlider(
+            id="filter-slider-A",
+            persistence=False,
+        ),
+        html.P(id="filter-callback-A"),
+        dcc.Markdown("Select a **secondary** feature to filter:"),
+        dcc.Dropdown(
+            id="filter-select-B",
+            options=[
+                {"label": key, "value": key}
+                for key in df.columns if df[key].dtype == 'float64'
+            ],
+            value=df.columns[0],
+            persistence=True,
+        ),
+        dcc.RangeSlider(
+            id="filter-slider-B",
+            persistence=False,
+        ),
+        html.P(id="filter-callback-B"),
+        dcc.Graph(id="filter-fig"),
+        html.P("Select a simulation run"),
+        dcc.Dropdown(
+            id="filtered-experiment-select",
+            persistence=True,
+        ),
+        html.P("Select a component to assess"),
+        dcc.Dropdown(
+            id="selected-component",
+            value=None,
+            persistence=False,
+        ),
+        dbc.Table(id="component-table"),
+        dcc.Graph(id="hourly"),
+        html.P(
+            ["Browse through the year per weeknumber below:"],
+            style={"padding-left": "5%"},
+        ),
+        dcc.Slider(
+            id="startingweek",
+            min=min(af.weeks.values()),
+            max=max(af.weeks.values()),
+            value=af.startingweek,
+            persistence=False,
+        ),
+        dcc.Store(
+            id="data-store",
+            data=dict(),
+        ),
+        dcc.Store(
+            id="store-filtered-df",
+            data=dict(),
+        ),
+    ],
+    className="container",
+)
+
+## reads associated JSON file and stores to react app
 ## (this allows for sharing between callbacks)
 @app.callback(
     Output("data-store", "data"),
-    Input("selected-model", "value"),
+    Input("filtered-experiment-select", "value"),
 )
 def data_store(selected_model):
 
@@ -77,59 +111,143 @@ def data_store(selected_model):
             data = json.load(json_file)
         return data
 
+## Filter A
+@app.callback(
+    Output("filter-slider-A", "min"),
+    Output("filter-slider-A", "max"),
+    Output("filter-slider-A", "value"),
+    Output("filter-slider-A", "marks"),
+    Output("filter-slider-A", "step"),
+    Input("filter-select-A", "value"),
+)
+def filter_a(feature):
+    minn = df[feature].min()
+    maxx = df[feature].max()
+    value = [minn, maxx]
+    marks = {i: str(round(i,2)) for i in value}
+    step = (maxx - minn)/100
+    return minn, maxx, value, marks, step
+
+## Filter B
+@app.callback(
+    Output("filter-slider-B", "min"),
+    Output("filter-slider-B", "max"),
+    Output("filter-slider-B", "value"),
+    Output("filter-slider-B", "marks"),
+    Output("filter-slider-B", "step"),
+    Input("filter-select-B", "value"),
+)
+def filter_a(feature):
+    minn = df[feature].min()
+    maxx = df[feature].max()
+    value = [minn, maxx]
+    marks = {i: str(round(i,2)) for i in value}
+    step = (maxx - minn)/100
+    return minn, maxx, value, marks, step
+
+## Slider A
+@app.callback(
+    Output("filter-callback-A", "children"),
+    Input("filter-slider-A", "value"),
+)
+def slider_callback(value):
+    value = [round(val,3) for val in value]
+    return f"Selected values {value}"
+## Slider B
+@app.callback(
+    Output("filter-callback-B", "children"),
+    Input("filter-slider-B", "value"),
+)
+def slider_callback(value):
+    value = [round(val,3) for val in value]
+    return f"Selected values {value}"
+
+# visualize the filter
+@app.callback(
+    Output("filter-fig", "figure"),
+    Output("store-filtered-df", "data"),
+    Input("filter-select-A", "value"),
+    Input("filter-select-B", "value"),
+    Input("filter-slider-A", "value"),
+    Input("filter-slider-B", "value"),
+)
+def filter_figure(x_col, y_col, x_range, y_range):
+    
+    xmin, xmax = x_range
+    ymin, ymax = y_range
+    
+    x_slice = (df[x_col] > xmin) & (df[x_col] < xmax)
+    y_slice = (df[y_col] > ymin) & (df[y_col] < ymax)
+    total_slice = x_slice & y_slice
+    sliced_df = df[total_slice]
+    fig = go.Figure(data=px.scatter(
+        sliced_df,
+        x=x_col,
+        y=y_col
+    )
+    )
+    fig.update_layout(
+        template="simple_white"
+        )
+    fig.update_xaxes(range=(df[x_col].min()*1.05, df[x_col].max()*1.05))
+    fig.update_yaxes(range=(df[y_col].min()*1.05, df[y_col].max()*1.05))
+
+    data = sliced_df.to_json()
+    return fig, data
+
+## populate dropdown based on selection
+@app.callback(
+    Output("filtered-experiment-select", "options"),
+    Output("filtered-experiment-select", "value"),
+    Input("store-filtered-df", "data"),
+)
+def populate_filtered_experiments(data):
+    
+    df = pd.DataFrame.from_dict(json.loads(data))
+    filenames_strs = df.filename_export
+
+    options=[
+        {"label": filename, "value": str((FOLDER / filename).absolute())}
+        for filename in filenames_strs
+    ]
+    value= str((FOLDER / filenames_strs[0]).absolute())
+    print(len(options))
+    return options, value
+
 ## hourly profile plot
 @app.callback(
-    Output("hourly", "figure"), 
+    Output("hourly", "figure"),
     Input("startingweek", "value"),
     Input("data-store", "data"),
 )
 def profile_plot(startingweek, data):
-    
+
     fig = af.make_profile_plot(startingweek, data)
 
     return fig
 
-## second figure callback
-@app.callback(
-    Output("figure-2", "figure"), 
-    Output("figure-2-title", "children"),
-    Input("data-store", "data"),
-    Input("selected-figure-2", "value"),
-)
-def figure2(data, selected_fig):
-    if selected_fig is not None:
-        
-        fig_factory = af.figure_2_options[selected_fig]['function']
-        fig = fig_factory(data)
-        title = af.figure_2_options[selected_fig]['display-text']
-        
-        return fig, title
 
 ## component browser dropdown populator
-@app.callback(
-    Output("selected-component", "options"),
-    Input("data-store", "data")
-)
+@app.callback(Output("selected-component", "options"), Input("data-store", "data"))
 def component_dropdown(data):
-
-    name = lambda key: data[key]['name'] + f' ({key})'
-    options = [{'label': name(key), 'value': key}
-                for key in data.keys()]
-    options.insert(0,{'label': 'None', 'value': 'null'})
+    data = AttrDict(data)
+    name = lambda key: data.components[key]["name"] + f" ({key})"
+    options = [{"label": name(key), "value": key} for key in data.components.keys()]
+    options.insert(0, {"label": "None", "value": "null"})
 
     return options
 
 ## component table callback
 @app.callback(
-    Output("component-table","children"),
-    Input("selected-component","value"),
+    Output("component-table", "children"),
+    Input("filtered-experiment-select", "value"),
     Input("data-store", "data"),
 )
 def component_table(ckey, data):
-    
+
     table = af.make_component_table(ckey, data)
-    
+
     return table
 
 if __name__ == "__main__":
-   app.run_server(debug=True)
+    app.run_server(debug=True)

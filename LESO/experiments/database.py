80fdf058e6b664b5506bc8905bc6319d6e895fda
thesethtruth <s.w.p.vanwieringen@gmail.com>
2021-10-13 20:20:29 +0200

    added a mongodb to dataframe feauture in LESO.experiments

--- LESO/experiments/database.py
+++ LESO/experiments/database.py
@@ -1,17 +1,24 @@
 import pymongo
 import ssl
-from .password import beast as password
+import pandas as pd
+from .password import beast
 
+PASSWORD = beast
+USER = 'beast'
 
-def insert_entry_in_mongo(database: str, collection: str, entry: dict):
+def insert_entry_in_mongo(database: str, collection: str, entry: dict, username=None, password=None):
     """
     Method for writing data to the mongo cluster.
         database: ["experiments"] description of the run of experiments
         collection: ["nametag"] the id entered at the start of the ema experiments
         entry: [db_entry] the data to be pushed to the cluster
     """
-
-    connect_string = f"mongodb+srv://beast:{password}@freeclusterthesis.k6h1x.mongodb.net/experiments?retryWrites=true&w=majority"
+    if username is None:
+        username = USER
+    if password is None:
+        password = PASSWORD
+    
+    connect_string = f"mongodb+srv://{username}:{password}@freeclusterthesis.k6h1x.mongodb.net/experiments?retryWrites=true&w=majority"
     client = pymongo.MongoClient(connect_string, ssl_cert_reqs=ssl.CERT_NONE)
 
     db = client[database]
@@ -29,3 +36,34 @@ def send_ema_exp_to_mongo(collection, entry):
     return insert_entry_in_mongo(
         database="experiments", collection=collection, entry=entry
     )
+
+def read_mongo(database: str, collection: str, query={}, username=None, password=None, no_id=True):
+    """ Read from Mongo and Store into DataFrame """
+
+    # Connect to MongoDB
+    db = _connect_mongo(database=database, username=username, password=password)
+
+    # Make a query to the specific DB and Collection
+    cursor = db[collection].find(query)
+
+    # Expand the cursor and construct the DataFrame
+    df =  pd.DataFrame(list(cursor))
+
+    # Delete the _id
+    if no_id:
+        del df['_id']
+
+    return df
+
+def _connect_mongo(database: str, username=None, password=None):
+    if username is None:
+        username = USER
+    if password is None:
+        password = PASSWORD
+    
+    connect_string = f"mongodb+srv://{username}:{password}@freeclusterthesis.k6h1x.mongodb.net/experiments?retryWrites=true&w=majority"
+    client = pymongo.MongoClient(connect_string, ssl_cert_reqs=ssl.CERT_NONE)
+
+    db = client[database]
+
+    return db

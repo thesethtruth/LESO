484f4eb99cfbf8f7f3f90d1f9418d38d5a2cab36
thesethtruth <seth@uu-engineering.nl>
2021-10-22 13:19:31 +0200

    updated the cablepooling experiments cheap battery variant

--- thesis scripts/experiments/cablepool/ema/cablepool_ema_experiments_cheap_battery.py
+++ thesis scripts/experiments/cablepool/ema/cablepool_ema_experiments_cheap_battery.py
@@ -10,35 +10,33 @@ from ema_workbench import (
     MultiprocessingEvaluator,
     SequentialEvaluator,
 )
-from cablepool_leso_handshake import METRICS, RESULTS_FOLDER, CablePooling
+from cablepool_leso_handshake import METRICS, RESULTS_FOLDER, CablePooling, COLLECTION
+
 
 
 if __name__ == "__main__":
     
     # initiate model
     ema_logging.log_to_stderr(ema_logging.INFO)
-    APPROACH =  "cheap_battery"
-    run_ID = f"{APPROACH}_{input('Please enter the run ID:')}" 
+    
+    run_ID = input('Please enter the run ID:')
+    APPROACH = "cheap_battery"
     initialized_model = partial(CablePooling, run_ID=run_ID, approach=APPROACH)
-    model = Model(name="cablepoolingCheapBattery", function=initialized_model)
+    model = Model(name=f"{COLLECTION}", function=initialized_model)
 
     # uncertainties / scenarios
     model.uncertainties = [
         RealParameter("pv_cost_factor", 0.38, 0.85),
-        RealParameter("battery_cost_factor", 0.1, 0.41),
+        RealParameter("battery_cost_factor", 0.41, 0.70),
     ]
     # specify outcomes
     model.outcomes = [ScalarOutcome(metric) for metric in METRICS]
 
     # run experiments
-    with MultiprocessingEvaluator(model, n_processes=7) as evaluator:
-        results = evaluator.perform_experiments(scenarios=200)
+    with MultiprocessingEvaluator(model, n_processes=10) as evaluator:
+        results = evaluator.perform_experiments(scenarios=200, policies=1)
     
-    # with SequentialEvaluator(model) as evaluator:
-    #     results = evaluator.perform_experiments(scenarios=1)
-
     # save results
-    RESULTS_FOLDER.mkdir(parents=True, exist_ok=True)
-    results_file_name = RESULTS_FOLDER / f"cablepooling_ema_results_{run_ID}.tar.gz"
+    results_file_name = RESULTS_FOLDER / f"{COLLECTION}_ema_results_{run_ID}.tar.gz"
     save_results(results, file_name=results_file_name)
 

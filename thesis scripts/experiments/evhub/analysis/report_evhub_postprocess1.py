e1ed1f28bc4cce3ff88fdaa5cc60e16572b89d31
thesethtruth <seth@uu-engineering.nl>
2021-10-23 15:44:45 +0200

    evhub post processing 1

--- thesis scripts/experiments/evhub/analysis/report_evhub_postprocess1.py
+++ thesis scripts/experiments/evhub/analysis/report_evhub_postprocess1.py
@@ -5,74 +5,50 @@ from pathlib import Path
 import seaborn as sns
 import matplotlib.pyplot as plt
 
-from LESO.experiments.analysis import (
-    load_ema_leso_results,
-    open_leso_experiment_file,
-)
+
 from LESO.plotting import default_matplotlib_save, default_matplotlib_style
+from evhub_postprocess_tools import get_data_from_db
 
-#%% constants
+#%% paths
 FOLDER = Path(__file__).parent
-FIG_FOLDER = FOLDER / "images"
 RESULT_FOLDER = FOLDER.parent / "results"
+IMAGE_FOLDER = FOLDER / "images"
+RESOURCE_FOLDER = FOLDER / "resources"
+RESOURCE_FOLDER.mkdir(exist_ok=True)
+
+## constants
 LEFT_MARGIN = 0.15
+COLLECTION = "evhub"
+RUN_ID = "2210_v2"
 
-run_id = 210907
-
-#%% load in results
-experiments, outcomes, db = load_ema_leso_results(
-    run_id=run_id, exp_prefix="evhub", results_folder=RESULT_FOLDER
-)
-
-## note that for 21 cases the optimizer did not exit sucessfully. No clue on how to deal with this. --> ignore it is
+## settings
+filter = ("run_id", "=", RUN_ID)
 
 ## pointers
 pv_col = "PV South installed capacity"
 wind_col = "Nordex N100 2500 installed capacity"
-bat_col = "2h battery installed capacity"
+bat2_col = "battery installed capacity"
+bat6_col = "6h battery installed capacity"
+bat10_col = "10h battery installed capacity"
+total_bat_col = "total_storage_energy"
+batcols = [bat2_col, bat6_col, bat10_col]
+bivar_tech_dict = {"PV": pv_col, "wind": wind_col, "battery": total_bat_col}
 
-bivar_tech_dict = {"PV": pv_col, "wind": wind_col, "battery": bat_col}
 
+#%% load in results
+db = get_data_from_db(
+    collection=COLLECTION,
+    run_id=RUN_ID,
+    filter=filter,
+    force_refresh=False
+)
+
+#%% plotting
 grid_capacities = [0.5, 1.0, 0.0, 1.5]
 for grid_cap in grid_capacities:
     ## data selection
 
     df = db.query(f"grid_capacity == {grid_cap}").copy(deep=True)
-    exp = open_leso_experiment_file(RESULT_FOLDER / df.filename_export.iat[113])
-
-    ## needed for more kpis
-    spec_yield_pv = (
-        sum(exp.components.pv2.state["power [+]"])
-        / exp.components.pv2.settings.installed
-    )
-    tot_yield_wind = sum(exp.components.wind1.state["power [+]"])
-
-    ## change / add some data
-    df["pv_cost_absolute"] = df.pv_cost_factor * 1020
-    df["wind_cost_absolute"] = df.wind_cost_factor * 1350
-    df["curtailment"] = -df["curtailment"]
-    df["total_generation"] = df[pv_col] * spec_yield_pv + tot_yield_wind
-    df["relative_curtailment"] = df["curtailment"] / df["total_generation"] * 100
-    df["total_installed_capacity"] = df[pv_col] + df[wind_col]
-
-    def linear_map(
-        value,
-    ):
-        min, max = 0.41, 0.70  # @@
-        map_min, map_max = 0.42, 0.81  # @@
-
-        frac = (value - min) / (max - min)
-        m_value = frac * (map_max - map_min) + map_min
-
-        return m_value
-
-    power_ref = 257
-    storage_ref = 277
-
-    df["battery_cost_absolute_2h"] = [
-        (bcf * storage_ref * 2 + linear_map(bcf) * power_ref) / 2
-        for bcf in df["battery_cost_factor"].values
-    ]
 
     #%% PV Deployment vs absolut cost scatter
 
@@ -106,7 +82,7 @@ for grid_cap in grid_capacities:
     plt.setp(l.get_title(), multialignment="center")
 
     default_matplotlib_save(
-        fig, FIG_FOLDER / f"report_evhub_grid-{grid_cap}_pv_deployment_vs_cost.png"
+        fig, IMAGE_FOLDER / f"report_evhub_grid-{grid_cap}_pv_deployment_vs_cost.png"
     )
 
     #%% Battery deployment vs absolut cost scatter
@@ -115,8 +91,8 @@ for grid_cap in grid_capacities:
     fig, ax = default_matplotlib_style(fig, ax)
     fig.set_size_inches(6, 2.2)
     sns.scatterplot(
-        x="battery_cost_absolute_2h",
-        y=bat_col,
+        x="battery_power_cost_absolute",
+        y=total_bat_col,
         size="total_installed_capacity",
         hue="total_installed_capacity",
         data=df,
@@ -142,7 +118,7 @@ for grid_cap in grid_capacities:
 
     default_matplotlib_save(
         fig,
-        FIG_FOLDER / f"report_evhub_grid-{grid_cap}_battery_deployment_vs_cost.png",
+        IMAGE_FOLDER / f"report_evhub_grid-{grid_cap}_battery_deployment_vs_cost.png",
         adjust_left=LEFT_MARGIN,
     )
 
@@ -178,7 +154,7 @@ for grid_cap in grid_capacities:
     plt.setp(l.get_title(), multialignment="center")
 
     default_matplotlib_save(
-        fig, FIG_FOLDER / f"report_evhub_grid-{grid_cap}_wind_deployment_vs_cost.png"
+        fig, IMAGE_FOLDER / f"report_evhub_grid-{grid_cap}_wind_deployment_vs_cost.png"
     )
 
     #%% relative curtailment
@@ -189,7 +165,7 @@ for grid_cap in grid_capacities:
     sns.scatterplot(
         x="total_installed_capacity",
         y="relative_curtailment",
-        hue=bat_col,
+        hue=total_bat_col,
         data=df,
         palette="Blues",
         ax=ax,
@@ -207,12 +183,12 @@ for grid_cap in grid_capacities:
         loc=9,
         borderaxespad=0.0,
         frameon=True,
-        title="deployed battery capacity (MW)",
+        title="deployed battery capacity (MWh)",
         ncol=3,
     )
 
     default_matplotlib_save(
-        fig, FIG_FOLDER / f"report_evhub_grid-{grid_cap}_curtailment_vs_deployment.png"
+        fig, IMAGE_FOLDER / f"report_evhub_grid-{grid_cap}_curtailment_vs_deployment.png"
     )
 
     #%% absolute curtailment
@@ -223,7 +199,7 @@ for grid_cap in grid_capacities:
     sns.scatterplot(
         x="total_installed_capacity",
         y="curtailment",
-        hue=bat_col,
+        hue=total_bat_col,
         data=df,
         palette="Blues",
         ax=ax,
@@ -241,13 +217,13 @@ for grid_cap in grid_capacities:
         loc=9,
         borderaxespad=0.0,
         frameon=True,
-        title="deployed battery capacity (MW)",
+        title="deployed battery capacity (MWh)",
         ncol=3,
     )
 
     default_matplotlib_save(
         fig,
-        FIG_FOLDER / f"report_evhub_grid-{grid_cap}_abs_curtailment_vs_deployment.png",
+        IMAGE_FOLDER / f"report_evhub_grid-{grid_cap}_abs_curtailment_vs_deployment.png",
     )
 
 
@@ -261,7 +237,7 @@ for grid_cap in grid_capacities:
 
         sns.scatterplot(
             x="pv_cost_absolute",
-            y="battery_cost_absolute_2h",
+            y="battery_power_cost_absolute",
             size=tech_col,
             hue=tech_col,
             data=df,
@@ -271,7 +247,7 @@ for grid_cap in grid_capacities:
             edgecolor="black",
         )
 
-        ax.set_ylabel("2h battery \n capacity cost (€/kWh)")
+        ax.set_ylabel("battery \n capacity cost (€/kW)")
         # ax.set_ylim([160, 310])
 
         ax.set_xlabel("PV capacity cost (€/kWp)")
@@ -287,7 +263,7 @@ for grid_cap in grid_capacities:
         )
 
         default_matplotlib_save(
-            fig, FIG_FOLDER / f"report_evhub_grid-{grid_cap}_bivariate_pv_vs_bat_{tech}.png"
+            fig, IMAGE_FOLDER / f"report_evhub_grid-{grid_cap}_bivariate_pv_vs_bat_{tech}.png"
         )
 
     
@@ -299,7 +275,7 @@ for grid_cap in grid_capacities:
 
         sns.scatterplot(
             x="wind_cost_absolute",
-            y="battery_cost_absolute_2h",
+            y="battery_power_cost_absolute",
             size=tech_col,
             hue=tech_col,
             data=df,
@@ -309,7 +285,7 @@ for grid_cap in grid_capacities:
             edgecolor="black",
         )
 
-        ax.set_ylabel("2h battery \n capacity cost (€/kWh)")
+        ax.set_ylabel("battery \n capacity cost (€/kW)")
         # ax.set_ylim([160, 310])
 
         ax.set_xlabel("wind capacity cost (€/kW)")
@@ -326,7 +302,7 @@ for grid_cap in grid_capacities:
 
         default_matplotlib_save(
             fig,
-            FIG_FOLDER
+            IMAGE_FOLDER
             / f"report_evhub_grid-{grid_cap}_bivariate_wind_vs_bat_{tech}.png",
         )
 
@@ -365,6 +341,6 @@ for grid_cap in grid_capacities:
 
         default_matplotlib_save(
             fig,
-            FIG_FOLDER
+            IMAGE_FOLDER
             / f"report_evhub_grid-{grid_cap}_bivariate_wind_vs_pv_{tech}.png",
         )

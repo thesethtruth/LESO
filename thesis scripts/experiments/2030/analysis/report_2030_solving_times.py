31e4cfa12ee197354251664da241668133aa0372
thesethtruth <seth@uu-engineering.nl>
2021-10-27 20:11:20 +0200

    solver settings analysis

--- thesis scripts/experiments/2030/analysis/report_2030_solving_times.py
+++ thesis scripts/experiments/2030/analysis/report_2030_solving_times.py
@@ -38,7 +38,7 @@ selection = (df
 
 
 ## rerun with 0 crossover, 2 method and BarConvTol e-5
-run_id_rerun_cases = "test_longsolve"
+run_id_rerun_cases = "test_longsolve3"
 filters = [
     ("run_id", "=", run_id_rerun_cases),
     ("target_RE_strategy", "=", "fixed_target_100"),
@@ -48,21 +48,23 @@ rerun_df = rerun_df.sort_values("hydrogen_cost_factor", ascending=False)
 
 #%% compare data
 capacity_columns = [col for col in rerun_df if "installed capacity" in col]
+capacity_columns.insert(0, "source")
+capacity_columns.insert(0, "row_id")
 compare_df = pd.DataFrame(columns=capacity_columns)
 for name, row in selection.iterrows():
     
-    row_id = row.filename_export.replace(".json", "")
+    row_id = row.filename_export.replace(".json", "").replace("gld2030_exp_", "")
     matched_rerun = rerun_df[
         rerun_df.hydrogen_cost_factor == row.hydrogen_cost_factor
-    ]
+    ].copy()
+    matched_rerun.loc[:,"source"] = "retro"
+    matched_rerun.loc[:,"row_id"] = row_id
     matched_rerun = matched_rerun.iloc[0,:][capacity_columns]
-
-    compare_df.loc[row_id+"_original", :] = row[capacity_columns]
-    compare_df.loc[row_id+"_retro", :] = matched_rerun
-
-
-
-
+    row.loc["source"] = "original"
+    row.loc["row_id"] = row_id
+    compare_df.loc[name, :] = row[capacity_columns]
+    compare_df.loc[str(name)+"_retro", :] = matched_rerun
+compare_df.to_clipboard()
 #%%
 print_solving_times = False
 if print_solving_times:
